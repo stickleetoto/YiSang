391440f1ccf996fc030ef96eab2c9dea84aa37e3
stockman <stickee090@gmail.com>
2026-09-19 11:53:22 +0900

    test: import socket for 100-continue regression

--- tests/test_model_server_http.py
+++ tests/test_model_server_http.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import socket
 from threading import Thread
 from urllib import error as urllib_error
 from urllib import request as urllib_request
