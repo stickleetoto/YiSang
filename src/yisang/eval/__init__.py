4bdf43621728f1f7db005bebbe8d2b08d5eef21b
stockman <stickee090@gmail.com>
2026-09-19 13:11:46 +0900

    feat: export repeatability evaluation types

---
+++ src/yisang/eval/__init__.py
@@ -0,0 +1,21 @@
+from .repeatability import (
+    BaselineGate,
+    CaseMetrics,
+    RepeatabilityReport,
+    RunMetrics,
+    aggregate_case,
+    build_report,
+    evaluate_baseline,
+    run_case,
+)
+
+__all__ = [
+    "BaselineGate",
+    "CaseMetrics",
+    "RepeatabilityReport",
+    "RunMetrics",
+    "aggregate_case",
+    "build_report",
+    "evaluate_baseline",
+    "run_case",
+]
